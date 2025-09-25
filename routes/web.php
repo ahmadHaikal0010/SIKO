98464edde6e4360528d678eeab033b711b1ebbc6
Ahmad Haikal <haikala0010@gmail.com>
2025-09-25 13:14:04 +0700

    feature logout, membuat fitur agar pengguna bisa logout

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@ Route::get('/loginform', fn() => view('auth.login'))->name('loginform');
 
 // Authentication Routes
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
