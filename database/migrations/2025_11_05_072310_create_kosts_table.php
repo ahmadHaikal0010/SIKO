0ec4266f4d67ac8de5e9b8d2c0ab9eeb318ab87b
Ahmad Haikal <haikala0010@gmail.com>
2025-11-05 14:39:47 +0700

    feat: Melihat Informasi Kost - Membuat Model Kost - Controller Kost - Migration Kost - Route untuk index Kost

---
+++ database/migrations/2025_11_05_072310_create_kosts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kosts', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kost');
+            $table->text('deskripsi');
+            $table->string('fasilitas');
+            $table->text('alamat');
+            $table->string('total_kamar');
+            $table->string('harga_kost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kosts');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\KostController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\IsAdmin;
@@ -12,10 +13,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Tenant Routes
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified', IsPenghuni::class])->name('dashboard');
 
+// Authenticated User Routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -27,4 +30,5 @@ require __DIR__ . '/auth.php';
 // Admin Routes
 Route::prefix('admin')->name('admin.')->middleware(['auth', IsAdmin::class])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/informasi-kost', [KostController::class, 'index'])->name('kost.index');
 });
